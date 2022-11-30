05764964120bacae93d60c65119bee9f3960de5f
SiyuXiao8 <siyuxiao8@gmail.com>
2022-11-30 10:41:48 -0500

    1 st commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ani Web Project</h1>
+
+    </div>
+  );
+}
+// navigation side bar - search, profile, sign up, register, logout
+// display lists of anime to anonymous user
+
+export default App;
