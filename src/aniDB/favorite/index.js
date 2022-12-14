a311ff2527f6539a5fdfc8d3cdbe0c0a28064335
SiyuXiao8 <siyuxiao8@gmail.com>
2022-12-13 21:21:47 -0500

    favorite list height set to auto

--- src/aniDB/favorite/index.js
+++ src/aniDB/favorite/index.js
@@ -1,6 +1,7 @@
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import {deleteLikeThunk, findLikeByUserThunk} from "./favorite-thunk";
+import '../home/index.css'
 
 const FavoriteAnime = () => {
     const {favorites} = useSelector(state => state.favoritesData)
@@ -15,13 +16,13 @@ const FavoriteAnime = () => {
             <h3> <span className='text-info'>{currentUser.username}</span> Favorite list</h3>
             {favorites && currentUser &&
                 favorites.map(f=>
-                    <div key={f._id} className="card d-inline-block me-3 mb-2 rounded-top" style={{width: '18rem'}}>
-                        <img src={f.animeImage} className="card-img-top" alt={f.animeName}/>
+                    <div key={f._id} className="card d-inline-block me-3 mb-3 rounded-top card-size" style={{height: 'auto'}}>
+                        <img src={f.animeImage} className="card-img-top img-size" alt={f.animeName}/>
                         <div  className="card-body">
                             <h5 className="card-title">
                                 {}
-                                {f.animeName.length > 25 ?
-                                    (`${f.animeName.substring(0, 25)}...`) : (f.animeName)
+                                {f.animeName.length > 15 ?
+                                    (`${f.animeName.substring(0, 15)}...`) : (f.animeName)
                                 }
                             </h5>
                             <button onClick={()=>{
